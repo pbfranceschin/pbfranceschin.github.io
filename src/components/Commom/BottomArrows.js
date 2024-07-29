b66439bbbd43c6b899d73de6d405b3e441beb43e
Pedro Franceschin <pbfranceschin@gmail.com>
2024-07-29 08:25:16 -0300

    BottomArrows: hide back arrow on desktop

--- src/components/Commom/BottomArrows.js
+++ src/components/Commom/BottomArrows.js
@@ -1,13 +1,14 @@
 import { BackArrow, UpArrow } from './Arrow';
 import './Common.css';
+import classNames from 'classnames';
 
 
 export const BottomArrows = () => {
 
     return (
         <div className='bottomArrowsContainer'>
+            <div className={classNames('bottomArrows', 'desktopHidden')}><BackArrow/></div>
             <div className='bottomArrows'><UpArrow/></div>
-            <div className='bottomArrows'><BackArrow/></div>
         </div>
     )
 }
