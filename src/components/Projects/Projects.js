b9297ee84b6bda6b9fe523f5d07423ed23326dd9
Pedro Franceschin <pbfranceschin@gmail.com>
2024-09-02 09:39:08 -0300

    Projects: text content

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,7 @@ export default function Projects({ setPage }) {
                 <div className='projectHeaderLeft'>
                     <h3>Graphia</h3>
                 </div>
-                <p><span className='label'>About:</span> protocol that leverages blockchain to enable automatic and recursive funding distribution based on a cross-reference tree. The main cases being studied right now are ‘open source software’ (possibly including package manager and github integration), publishing (scientific and otherwise) and music streaming. The core concept is that instances of work that receive funding can share the funding with other works that had relevant contribution in a programmatic fashion.</p>
+                <p><span className='label'>About:</span> protocol that leverages blockchain to enable automatic and recursive funding distribution based on a cross-reference tree. The main cases being studied right now are ‘open source software’ (possibly including package manager and github integration), publishing (scientific and otherwise) and music streaming. The core concept is that instances of work that receive funding can share the funding, in a programmatic fashion, with other works that had relevant contribution.</p>
                 <p><span className='label'>Stage:</span> early development.</p>
                 <p><span className='label'>Team:</span> Australopitech.</p>
             </div>
